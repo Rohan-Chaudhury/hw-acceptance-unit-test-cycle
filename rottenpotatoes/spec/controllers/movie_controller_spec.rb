083c9506fe0e9c0e7f805030a0775dfb5ef04b2f
EC2 Default User <ec2-user@ip-172-31-5-173.us-east-2.compute.internal>
2021-10-15 03:07:28 +0000

    Part 3

--- rottenpotatoes/spec/controllers/movie_controller_spec.rb
+++ rottenpotatoes/spec/controllers/movie_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe MoviesController do
     
-    describe 'get Movies with same Director function' do
+    describe 'find Movies with same Director function' do
         let!(:movie_1) { Movie.create!(title: 'Star Wars', director: 'George Lucas') }
         let!(:movie_2) { Movie.create!(title: 'Blade Runner', director: 'Ridley Scott') }
         let!(:movie_3) { Movie.create!(title: 'Alien') }
@@ -21,9 +21,22 @@ describe MoviesController do
             expect(movies).to eql(['Star Wars', 'THX-1138'])
         end
         
+    
+        it 'should not assign rest of the movies' do
+            get :similar, id: movie_1.id
+            
+            movies=[]
+            
+            for i in assigns(:sim_movies) 
+                movies.append(i.title)
+            end
+
+            expect(movies).not_to eql(['Blade Runner', 'Alien'])
+        end
         
+    
         
-        it 'should display the similar.html.erb template if director exists' do
+        it 'should display the similar.html.erb template of the movie if director exists' do
             get :similar, id: movie_1.id
             expect(response).to render_template('similar')
         end
@@ -36,7 +49,7 @@ describe MoviesController do
         end
     end
 
-    describe 'get index function' do
+    describe 'index function' do
         let!(:movie_1) { Movie.create!(title: 'Star Wars', director: 'George Lucas') }
         let!(:movie_2) { Movie.create!(title: 'Blade Runner', director: 'Ridley Scott') }
         let!(:movie_3) { Movie.create!(title: 'Alien') }
@@ -62,7 +75,7 @@ describe MoviesController do
 
 
 
-    describe 'get show function' do
+    describe 'show function' do
         let!(:movie) { Movie.create!(title: 'Star Wars', director: 'George Lucas')}
         
         it 'should get the movie to show' do
@@ -72,7 +85,7 @@ describe MoviesController do
         
         
 
-        it 'should render show.html.erb template' do
+        it 'should render show.html.erb template of the movie' do
             get :show, id: movie.id
             expect(response).to render_template('show')
         end
@@ -81,7 +94,7 @@ describe MoviesController do
     
 
     
-    describe 'get new function' do
+    describe 'new function' do
         let!(:movie) { Movie.new }
         it 'should display new.html.erb template' do
             get :new
@@ -89,9 +102,12 @@ describe MoviesController do
         end
     end
     
-    describe 'post create function' do
+    describe 'create function' do
+        
         let!(:movie_1) { Movie.create!(title: 'Alien') }
+        
         let!(:movie_2) { Movie.create!(title: 'THX-1138', director: 'George Lucas') }
+        
         it 'should create a new movie' do
             post :create, movie: {title: 'Star Wars', director: 'George Lucas'}
             expect(Movie.count).to eql(3)
@@ -102,4 +118,91 @@ describe MoviesController do
         end
     end
 
+    describe 'edit function' do
+        
+        let!(:movie) {  Movie.create!(title: 'Star Wars', director: 'George Lucas') }
+        
+        
+        it 'should find the movie to edit' do
+            
+            get :edit, id: movie.id
+            
+            expect(@controller.instance_variable_get(:@movie)).to eql(movie)
+        end
+        
+        it 'should render edit.html.erb template of the movie' do
+            
+            get :edit, id: movie.id
+            
+            expect(response).to render_template('edit')
+        end
+    end
+    
+    describe 'update function' do
+        
+        let(:movie) {  Movie.create!(title: 'Star Wars', director: 'George Lucas') }
+        
+        
+        it 'should get the movie to update' do
+            
+            put :update, id: movie.id, movie:  {title: 'Blade Runner'}
+            
+            expect(@controller.instance_variable_get(:@movie)).to eql(movie)
+        end
+
+        
+        
+        it 'should update an existing movie title data' do
+            
+            put :update, id: movie.id, movie:  {title: 'Blade Runner'}
+            
+            movie.reload
+            
+            expect(movie.title).to eql('Blade Runner')
+        end
+        
+        it 'should update an existing movie director data' do
+            put :update, id: movie.id, movie:  {director: 'Ridley Scott'}
+            movie.reload
+            expect(movie.director).to eql('Ridley Scott')
+        end
+
+   
+        it 'should redirect to the movie page' do
+            
+            put :update, id: movie.id, movie: {title: 'THX-1138', director: 'George Lucas'}
+            
+            expect(response).to redirect_to(movie_path(movie))
+        end
+    end
+    
+    describe 'destroy function' do
+        
+        let!(:movie_1) { Movie.create!(title: 'Star Wars', director: 'George Lucas') }
+        let!(:movie_2) { Movie.create!(title: 'Blade Runner', director: 'Ridley Scott') }
+        
+        
+        it 'should get the movie to delete' do
+            
+            delete :destroy, id: movie_1.id
+            
+            expect(@controller.instance_variable_get(:@movie)).to eql(movie_1)
+        end
+
+     
+        
+        it 'should delete the movie from the database' do
+            
+            expect { delete :destroy, id: movie_1.id}.to change(Movie, :count).by(-1)
+        end
+        
+        it 'should redirect to index page' do
+            
+            delete :destroy, id: movie_1.id
+            
+            expect(response).to redirect_to(movies_path)
+        end
+    end
+    
+    
 end
